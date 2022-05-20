d0287e9f5b6ff19065687933e0715962371c6d7e
dmittrey <dmitryzubakhin@mail.ru>
2022-05-21 02:15:40 +0300

    Add MBean for count points and conditionally notify
    MBean count all and inner points and notify when we have 2 bad hits in a row

---
+++ src/main/java/com/example/WebLab3/mbeans/PointCounter.java
@@ -0,0 +1,50 @@
+package com.example.WebLab3.mbeans;
+
+import com.example.WebLab3.entity.Hit;
+import com.example.WebLab3.interfaces.Validator;
+import com.example.WebLab3.utility.HitValidator;
+
+import javax.management.Notification;
+
+public class PointCounter implements PointCounterMBean {
+
+    private long hitsAmount = 0;
+    private long innerHitsAmount = 0;
+
+    private boolean isPreviousInner = true;
+
+    private long sequenceNumber = 0;
+
+    private final Validator<Hit> hitValidator = new HitValidator();
+
+    public void persistHit(Hit aHit) {
+        hitsAmount++;
+
+        if (hitValidator.validate(aHit).isValidationStatus()) {
+            innerHitsAmount++;
+
+            isPreviousInner = true;
+        } else {
+            if (!isPreviousInner) {
+
+                Notification notification = new Notification("someType", this, ++sequenceNumber,
+                        System.currentTimeMillis(),
+                        "Two negative hits happened");
+
+                sendNotification(notification);
+            }
+
+            isPreviousInner = false;
+        }
+    }
+
+    @Override
+    public Long countAllPoints() {
+        return hitsAmount;
+    }
+
+    @Override
+    public Long countInnerPoints() {
+        return innerHitsAmount;
+    }
+}
---
+++ src/main/java/com/example/WebLab3/mbeans/PointCounterMBean.java
@@ -0,0 +1,8 @@
+package com.example.WebLab3.mbeans;
+
+public interface PointCounterMBean {
+
+    Long countAllPoints();
+
+    Long countInnerPoints();
+}
